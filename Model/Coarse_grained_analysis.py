c8b6b043fb12bd4f125c55ba18430089a8570cff
Luc <zorrilla.luc.research@gmail.com>
2025-03-14 20:21:53 +0100

    Put a minus sign to the torque

--- Model/Coarse_grained_analysis.py
+++ Model/Coarse_grained_analysis.py
@@ -24,7 +24,7 @@ from datetime import datetime
 
 folder_name = "C:/Users/Luc/Documents/PhD_Large_files/RheoFlag/Model/Output/" #"StraightLine_PeriodicFlow_Radau/"
 
-id_filename = "20250314-075441845737"
+id_filename = "20250314-080307047307"
 
 metadata_filename = folder_name + 'metadata_' + id_filename +'.json'
 data_filename = folder_name + 'data_' + id_filename + '.csv'
--- Model/Coarse_grained_axoneme_functions.py
+++ Model/Coarse_grained_axoneme_functions.py
@@ -621,7 +621,7 @@ def g(t, X_tilde, Sp4, k0, Beta, taus_b, gamma, n_L=[0,0], m_L=0, Lambdas=0, Zet
     n_0 = [0,0] # No displacement at the base
     if k0 == np.inf:
         k0=0
-    m_0 = k0*X_tilde[2] # Rotation at the base is allowed --> Not sure about the sign
+    m_0 = -k0*X_tilde[2] # Rotation at the base is allowed --> Not sure about the sign
 
     ##################################################################
     ###### Solve the linear system with infinite basal stiffness #####
