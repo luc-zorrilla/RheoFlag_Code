75ea693e14dd22911f86f737d9b0b914d9b9302b
Luc <zorrilla.luc.research@gmail.com>
2025-04-24 12:47:59 +0200

    Added static analysis file for conveniency

--- Model/A02_Coarse_grained_axoneme.py
+++ Model/A02_Coarse_grained_axoneme.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
 
     ##############################
     # Shear viscosity activation #
-    tau_s_list = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
+    tau_s_list = [0] # [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
     ##############################
 
     ####################################
@@ -188,9 +188,9 @@ if __name__ == "__main__":
     # X_flow_field_list = [np.array([0, 10**(-6)])]
     
     # Periodic vertical flow of amplitude ( max velocity) A and frequency w0: A*sin(t)
-    A_list = [2.5e-3] # 10**(np.linspace(-9, 0, 10)) # [2.5e-3]
+    A_list = [1e0, 1e1, 1e2, 2.5e2] # 10**(np.linspace(-9, 0, 10)) # [2.5e-3]
     n_w0 = 100
-    w0_list = 10**(-np.linspace(-3, 6, n_w0))
+    w0_list = [0] # 10**(-np.linspace(-3, 6, n_w0))
     w0 = 0 # 0 for constant flow, otherwise sinusoidal flow of period w0 in w_s units.
     psi = np.pi/2 # Angle of the flow w.r.t. the horizontal axis
 
@@ -209,12 +209,12 @@ if __name__ == "__main__":
     print("Setting time...")
 
     # time determined by the flow timescale
-    dT_list = [2*np.pi/(100*w0) for w0 in w0_list]
-    T_max_list = [2*np.pi*100/w0 for w0 in w0_list]
+    # dT_list = [2*np.pi/(100*w0) for w0 in w0_list]
+    # T_max_list = [2*np.pi*100/w0 for w0 in w0_list]
 
     # Same time for all simulations
-    # dT_list = [1e3 for w0 in w0_list]
-    # T_max_list = [1e6 for w0 in w0_list]
+    dT_list = [1e-3 for w0 in w0_list]
+    T_max_list = [1e1 for w0 in w0_list]
 
     T_span_list = [[0, T_max] for T_max in T_max_list]
     T_eval_list = [[dT_list[l]*i for i in range(int(T_max_list[l]/dT_list[l]))] for l in range(len(w0_list))]
@@ -223,7 +223,7 @@ if __name__ == "__main__":
 
     ########################################
     # Maximum simulation time (s) per step #
-    T_sim_max = 6000 # 10min
+    T_sim_max = 600
     ########################################
 
     print("Time set.")
