a3a06c67f9b636da9fa67249fc0f8538de24eed8
Luc <zorrilla.luc.research@gmail.com>
2025-06-20 18:03:40 +0200

    Added L-BFGS-B , Basin-Hopping, and found out that convergence is heavily dependent on w0 and the evaluated times.

--- Inference/Optimization_Viscoelastic_Model.py
+++ Inference/Optimization_Viscoelastic_Model.py
@@ -1,6 +1,7 @@
 ### Libraries
 import numpy as np
 import scipy.optimize as so
+from optimparallel import minimize_parallel # L-BFGS-B parallel implementation
 
 import plotly.express as px
 from plotly.subplots import make_subplots
@@ -14,7 +15,6 @@ sys.path.insert(1, 'c:\\Users\\Luc\\Documents\\MEGAsync\\PhD\\RheoFlag\\Code\\Mo
 from A01_Coarse_grained_axoneme_functions import *
 
 if __name__ == '__main__':
-    print("")
 
     #--------------------------------
     # Fix parameters
@@ -43,15 +43,15 @@ if __name__ == '__main__':
     Zetas = [Zeta]*N
 
     ### Time-dependent Flow field
-    A = 1e-5
-    w0 = 1e-1
+    A = 1e-2
+    w0 = 1e0
     psi = np.pi / 2
     Flow_field_filename = "" # Whether to use a measured flow field or not
     
     ## Integration and time
     method = 'BDF'
     dT = 2*np.pi/(10*w0)
-    T_max = 2*np.pi*10/w0
+    T_max = 2*np.pi*1/w0
     T_span = [0, T_max]
     T_eval = [dT*i for i in range(int(T_max/dT))]
     T_sim_max = 600
@@ -74,9 +74,9 @@ if __name__ == '__main__':
     # Minimization
 
     ## Set h_exp with simulation (or take from experimental data)
-    Sp4 = 1
+    Sp4 = 1.0
     h_exp = h(Sp4)
-    print("h_exp found. ")
+    print("h_exp found: ", h_exp.shape)
 
     ## Set a functional to be minimized
 
@@ -93,26 +93,44 @@ if __name__ == '__main__':
 
     ## Minimize J
 
-    ### Grid search
+    ### Grid search [Homemade]
     # C mor frero
 
-    ### BFGS (scipy)
-    x0 = 5
-    res = so.minimize(J, x0, method='BFGS', callback=callback_function, options={'disp': True} )
-
-    ### Basin-hopping + BFGS (scipy)
-    # minimizer_kwargs = {"method": "BFGS"}
+    ### BFGS [scipy]
     # x0 = 5
-    # ret = basinhopping(J, x0, minimizer_kwargs=minimizer_kwargs, niter=20)
-    # print("The global minimum is: ", ret.x, ret.fun)
+    # res = so.minimize(J, x0, method='BFGS', callback=callback_function, options={'disp': True} )
 
+    ### L-BFGS-B [scipy]
+    #### This method allows to specify "maxls", the max number of line search per iteration, can be parallelized (see below), and allows to add parameter bounds.
+    # epsilon = 1e-2
+    # x0 = 90 # 50 converges (to 1), but x0 >= 94 is stuck converging at x0.
+    # bounds = [(epsilon, x0+epsilon)]
+    # res = so.minimize(J, x0, method='L-BFGS-B', callback=callback_function, options={'disp': True}, bounds = bounds)
+
+    ### L-BFGS-B (parallel version) [optimparallel] --> NOT WORKING FOR NOW
+    # epsilon = 1e-2
+    # x0 = 90 # 50 converges (to 1), but x0 >= 94 is stuck converging at x0.
+    # bounds = [(epsilon, x0+epsilon)]
+    # res = minimize_parallel(fun = J, x0 = x0, bounds = bounds)    
+
+    ### Basin-hopping + BFGS (or any other local method) [scipy]
+    x0 = 90
+    epsilon = 1e-2
+    bounds = [(epsilon, 2*x0)]
+    minimizer_kwargs = {"method": "L-BFGS-B", "bounds": bounds, "options":{'disp': True},  "callback":callback_function}
+    ret = so.basinhopping(J, x0, minimizer_kwargs=minimizer_kwargs, niter=5)
+
+    print("ret.x", ret.x)
+    print("ret.success", ret.success)
+    print("ret.message", ret.message)
+    
+    res = ret.lowest_optimization_result 
     print("res.x", res.x)
     print("res.success", res.success)
     print("res.status", res.status)
     print("res.message", res.message)
-    print("res.fun, res.jac, res.hess_inv", res.fun, res.jac, res.hess_inv)
+    print("res.fun, res.jac, res.hess_inv operator, res.hess_inv @ res.x", "ress.hess_inv.todense()", res.fun, res.jac, res.hess_inv, res.hess_inv @ res.x, res.hess_inv.todense())
     print("res.nfev, res.njev, res.nit", res.nfev, res.njev, res.nit)
-
 
     #--------------------------------
     
