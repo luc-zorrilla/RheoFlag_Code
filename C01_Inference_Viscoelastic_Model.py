Fixed code, adding gradient and hessian computation after finding minimum (optional).

C01_Inference_Viscoelastic_Model.py
@@ -11,7 +11,7 @@ import multiprocessing as mp
 import pickle
 from pathlib import Path
 
-writing_path = (Path(__file__).resolve().parent.parent / 'Inference' / 'FromSimulationData' / 'BendingElasticity_BendingViscosity_Clamped' / 'QuarterPeriod')
+writing_path = (Path(__file__).resolve().parent.parent / 'Inference' / 'FromSimulationData' / 'BendingElasticity_NoViscosity_Clamped' / 'QuarterPeriod')
 from datetime import datetime
 import copy
 
@@ -141,6 +141,7 @@ def Basinhopping_LBFGSB_Scheme(func, guess_variables, bounds, niter = 0, T = 0, 
 
     x = x0
     f = func(x0)
+
     # # Gradient approximation can be optionnally computed here
     # # Hessian approximation can be optionnally computed here too
 
@@ -155,30 +156,55 @@ def Basinhopping_LBFGSB_Scheme(func, guess_variables, bounds, niter = 0, T = 0, 
 
         x = copy.deepcopy(intermediate_result.x)
         f = intermediate_result.fun
-        # Gradient approximation can be optionnally computed here
-        # Hessian approximation can be optionnally computed here too
-                
+        
         print("xk: ", x)
         print("f(xk): ", f)
 
+        # The following quantities should not be kept, as they increase computational time and memory. They are just used for testing.
+        # jac_compute = sd.jacobian(f = vec_func, x = x, step_direction = 1, maxiter = 10) # Gradient approximation
+        # print("jac_compute:", jac_compute)
+        # hess_compute = sd.hessian(f = vec_func, x = x) # Hessian approximation
+        # print("hess_compute: ", hess_compute)     
+
         X.append(x)
         F.append(f)
+        # dF.append(g)
+        # H.append(h)
 
         return
     
     minimizer_kwargs = {"method": method, "bounds": bounds, 'jac':'3-point', "options":{'disp': True, 'finite_diff_rel_step':1e-6}, "callback":callback_function}
     # Remark: an alternative is to set jac to None and to put an absolute step size eps in the options, like eps = 1e-6 for example.
     ret = so.basinhopping(func = func, x0 = x0, minimizer_kwargs = minimizer_kwargs, niter = niter, stepsize = stepsize, T = T)
+    x_final = ret.x
 
-    # Compute hessian at convergence point
-    # m = guess_variables.shape[0]  
-    # vec_func = Vectorize_Functional(func, m)
-    # h = sd.hessian(f = vec_func, x = ret.x).ddf
-    # ret.setdefault('hessian', h)    
+    # Compute gradient and hessian at convergence point
+    ## Check if on boundary and choose direction of gradient approximation accordingly (Directed Backward Difference [+-1] or Central Difference [0])
+    sl, sb = bounds.residual(x_final) # Lower and upper residual between x_final and the bounds
+    if np.all(sl > 0) & np.all(sb > 0): # Within bounds
+        step_direction = 0
+    elif np.all(sl==0):
+        step_direction = 1 # Lower boundary is reached
+    elif np.all(sb==0):
+        step_direction = -1 # Upper boundary is reached
+    else:
+        raise ValueError("x_final is outside of the domain. sl, sb = ", sl, sb)
+    
+    m = guess_variables.shape[0]
+    vec_func = Vectorize_Functional(func, m)
+    g = sd.jacobian(f = vec_func, x = x_final, step_direction = step_direction).df
+    print("g = ", g)
+    ret.setdefault('jacobian', g)
+    h = sd.hessian(f = vec_func, x = x_final).ddf
+    print("h = ", h)
+    ret.setdefault('hessian', h)
+
+    # Add a check to see, when the x_final has converged to a boundary, the gradient is actually going inside the space OR towards the boundary. In the latter case, restart the minimization scheme with x_final as the initial guess. But can an optimization be performed with a guess near the boundary? Or simply add an iteration to the basin-hopping algorithn. Or a check when nit <= 2 and when on a boundary, to perform another minimization.
 
     for V in [X, F, dF, H]:
         V = np.array(V)
-    
+
+    print("return basinhopping")
     return ret, X, F, dF, H
 
 ### Inference meta-function
@@ -336,6 +362,7 @@ def Viscoelastic_inference_inloop(flow_params, exp_params, guess_variable_params
 
     ###### Dictionary of the used function
     VI_dict = Make_Dict_From_Applied_Function(func = Viscoelastic_Inference, func_args = VI_args, func_output = [ret, X, F, dF, H])
+    print("VI_dict:", VI_dict)
 
     ###### Additional information
     VI_dict["exp_variable_params"] = exp_variable_params
@@ -397,22 +424,24 @@ if __name__ == '__main__':
         stepsize = 5
         opt_args = {"niter":niter, "T":T, "stepsize":stepsize}
 
-        for Sp4_guess in [10]:
+        for Sp4_guess in [1e1]:
             guess_variable_params = {"Sp4": Sp4_guess}
 
             ## Bounds
-            eps = 1e-3 # 1e-3
-            bound_Sp4 = [eps, 1e3]
+            Sp4_min = np.double(1e-6) # 1e-3
+            Sp4_max = np.double(1e6) # 1e3
+
+            bound_Sp4 = [Sp4_min, Sp4_max]
             # bound_tau_b = [0, 1e7]
             lb = [bound_Sp4[0]] #, bound_tau_b[0]]
             ub = [bound_Sp4[1]] #, bound_tau_b[1]]
             bounds = so.Bounds(lb,  ub)
 
             # Flow field
-            m1 = 7 # 11
-            A_vec = np.float_power(10, np.linspace(-8, -2, num = m1)) # np.array([1e-8])
-            m2 = 11 # 11
-            w0_vec = np.float_power(10, np.linspace(-10, 0, num = m2)) # np.array([1e-10])
+            m1 = 1 # 7
+            A_vec = np.array([1e-6]) # np.float_power(10, np.linspace(-8, -2, num = m1)) # np.array([1e-8])
+            m2 = 1 # 11
+            w0_vec = np.array([1e-10]) # np.float_power(10, np.linspace(-10, 0, num = m2)) # np.array([1e-10])
             m3 = 1 # 2
             psi_vec = np.array([np.pi/2]) # np.linspace(0, np.pi/2, num = m3)
 
@@ -459,7 +488,7 @@ if __name__ == '__main__':
             Flow_field_filename = "" # Whether to use a measured flow field or not
 
             # Start parallel computation
-            pool = mp.Pool(mp.cpu_count() - 2)
+            pool = mp.Pool(mp.cpu_count() - 1) # - 2)
 
             for i1 in range(m1):
                 A = A_vec[i1]

C02_VI_analysis.py
@@ -8,7 +8,7 @@ from misc_func import *
 import glob
 import pickle
 from pathlib import Path
-writing_path = (Path(__file__).resolve().parent.parent / 'Inference' / 'FromSimulationData' / 'BendingElasticity_BendingViscosity_Clamped' / 'QuarterPeriod' / 'jac_3point')
+writing_path = (Path(__file__).resolve().parent.parent / 'Inference' / 'FromSimulationData' / 'BendingElasticity_NoViscosity_Clamped' / 'QuarterPeriod')
 import numpy as np
 import pandas as pd
 
@@ -30,6 +30,7 @@ if __name__ == "__main__":
     F_list = []
     dF_list = []
     H_list = []
+    H_inv_list = []
     ret_list = []
 
     filepaths = list(writing_path.glob('**/*.pkl')) # List of path of .pkl files in the writing path
@@ -58,7 +59,7 @@ if __name__ == "__main__":
         X, F, dF, H = VI_dict["output"][1:]
         for l in range(4):
             V = np.array([X, F, dF, H][l]).squeeze()
-            V_list = [X_list, F_list, dF_list, H_list][l]
+            V_list = [X_list, F_list, dF_list, H_list, H_inv_list][l]
             V_list.append(V)
 
         IE = L2_relative_error(p_inf, p_star)
@@ -97,6 +98,7 @@ if __name__ == "__main__":
     df["F"] = F_list
     df["dF"] = dF_list
     df["H"] = H_list
+    df["H_inv"] = H_inv_list
     df["ret"] = ret_list
 
     n_vars = p_inf_list[0].shape[0]
@@ -110,24 +112,24 @@ if __name__ == "__main__":
     print(df)
 
     # Select for specific external parameters
-    df_Aw0 = df[(df['A'] == 1e-5) & (df['w0'] == 1e-6)].reset_index()
+    df_Aw0 = df[(df['A'] == 1e-6) & (df['w0'] == 1e-10)].reset_index()
 
     # Combine inferred parameters
 
-    # p_combined = [] # Combine parameter estimates (using BLC function) --> Not working when selecting for specific guess!
-    # for j in range(n_vars):
-    #     Z_vector_list_j = [np.array([df["p_inf"][k][j], df["Sigma"][k][j]]) for k in range(df["p_inf"].shape[0])]
-    #     Z_combined_vector_j = BLC(Z_vector_list_j)
-    #     p_combined.append(Z_combined_vector_j)
-    # print("p_combined", p_combined)
+    p_combined = [] # Combine parameter estimates (using BLC function) --> Not working when selecting for specific guess!
+    for j in range(n_vars):
+        Z_vector_list_j = [np.array([df["p_inf"][k][j], df["Sigma"][k][j]]) for k in range(df["p_inf"].shape[0])]
+        Z_combined_vector_j = BLC(Z_vector_list_j)
+        p_combined.append(Z_combined_vector_j)
+    print("p_combined", p_combined)
 
     # Plots
 
     # Plot X, F evolution for each A, w0
-    # fig = go.Figure()
-    # fig.add_scatter(x = np.arange(len(df_Aw0["X"][0])), y = df_Aw0["X"][0], name = "X")
-    # fig.add_scatter(x = np.arange(len(df_Aw0["F"][0])), y = df_Aw0["F"][0], name = "F")
-    # fig.vs_show()
+    fig = go.Figure()
+    fig.add_scatter(x = np.arange(len(df_Aw0["X"][0])), y = df_Aw0["X"][0], name = "X")
+    fig.add_scatter(x = np.arange(len(df_Aw0["F"][0])), y = df_Aw0["F"][0], name = "F")
+    fig.vs_show()
 
     ## Plot IE heatmap for each (A, w0)-point
     fig = go.Figure(data = go.Heatmap(x = np.log10(df['A']), y = np.log10(df['w0']), z = np.log10(df['IE']), colorscale = 'RdPu_r'))